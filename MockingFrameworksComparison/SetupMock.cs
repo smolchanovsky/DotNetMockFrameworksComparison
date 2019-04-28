75414cc4f0e84937ff9f8aa0f9196a3539ba7fc4
smolchanovsky <smolchanovsky@gmail.com>
2019-04-28 13:55:41 +0500

    add setup mocks

---
+++ MockingFrameworksComparison/SetupMock.cs
@@ -0,0 +1,45 @@
+﻿using FakeItEasy;
+using Fakes;
+using Moq;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MockingFrameworksComparison
+{
+	internal class SetupMock
+	{
+		protected Mock<IRepository> MoqRepository;
+		protected IRepository NsubRepository;
+		protected IRepository FieRepository;
+		protected Mock<IDbContext> MoqDbContext;
+		protected IDbContext NsubDbContext;
+		protected IDbContext FieDbContext;
+
+		[SetUp]
+		public void SetUpRepository()
+		{
+			//Moq
+			MoqRepository = new Mock<IRepository>();
+
+			//NSubstitute
+			NsubRepository = Substitute.For<IRepository>();
+
+			//FakeItEasy
+			FieRepository = A.Fake<IRepository>();
+		}
+
+		[SetUp]
+		public void SetUpDbContext()
+		{
+			//Moq
+			MoqDbContext = new Mock<IDbContext>();
+
+			//NSubstitute
+			NsubDbContext = Substitute.For<IDbContext>();
+
+			//FakeItEasy
+			FieDbContext = A.Fake<IDbContext>();
+		}
+	}
+}
+	
