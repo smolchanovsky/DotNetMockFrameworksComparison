05cf566224e62bcc1fc8e57e2e9c53243dc5909c
smolchanovsky <smolchanovsky@gmail.com>
2019-04-28 13:57:46 +0500

    add examples mock methods

---
+++ MockingFrameworksComparison/MockMethods/MockMethod.cs
@@ -0,0 +1,61 @@
+﻿using FakeItEasy;
+using Fakes;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MockingFrameworksComparison.MockMethods
+{
+	internal class MockMethod : SetupMock
+	{
+
+		[Test]
+		public void MockMethodWithoutArg()
+		{
+			//Moq
+			MoqRepository.Setup(x => x.GetAll()).Returns(new[] { new Entity(1) });
+
+			//NSubstitute
+			NsubRepository.GetAll().Returns(new[] { new Entity(1) });
+
+			//FakeItEasy
+			A.CallTo(() => FieRepository.GetAll()).Returns(new[] { new Entity(1) });
+
+			#region Assetion
+
+			MoqRepository.Object.GetAll().Should().BeEquivalentTo(new[] { new Entity(1) });
+
+			NsubRepository.GetAll().Should().BeEquivalentTo(new[] { new Entity(1) });
+
+			FieRepository.GetAll().Should().BeEquivalentTo(new[] { new Entity(1) });
+
+			#endregion
+		}
+
+		[Test]
+		public void MockMethodWithArg()
+		{
+			//Moq
+			MoqRepository.Setup(x => x.GetById(1)).Returns(new Entity(1));
+
+			//NSubstitute
+			NsubRepository.GetById(1).Returns(new Entity(1));
+
+			//FakeItEasy
+			A.CallTo(() => FieRepository.GetById(1)).Returns(new Entity(1));
+
+			#region Assetion
+
+			MoqRepository.Object.GetById(1).Should().BeEquivalentTo(new Entity(1));
+			MoqRepository.Object.GetById(0).Should().Be(null);
+
+			NsubRepository.GetById(1).Should().BeEquivalentTo(new Entity(1));
+			NsubRepository.GetById(0).Should().Be(null);
+
+			FieRepository.GetById(1).Should().BeEquivalentTo(new Entity(1));
+			FieRepository.GetById(0).Should().BeEquivalentTo(A.Fake<Entity>());
+
+			#endregion
+		}
+	}
+}
---
+++ MockingFrameworksComparison/MockMethods/MockMethodWithRefArg.cs
@@ -0,0 +1,51 @@
+﻿using FakeItEasy;
+using Fakes;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MockingFrameworksComparison.MockMethods
+{
+	internal class MockMethodWithRefArg : SetupMock
+	{
+		[Test]
+		public void MockWithRefArg()
+		{
+			//Moq
+			var moqRefEntity = new Entity(1);
+			MoqRepository
+				.Setup(x => x.Fill(ref moqRefEntity))
+				.Returns(true);
+
+			//NSubstitute
+			var nsubRefEntity = new Entity(1);
+			NsubRepository
+				.Fill(ref nsubRefEntity)
+				.Returns(true);
+
+			//FakeItEasy
+			var fieRefEntity = new Entity(1);
+			A.CallTo(() => FieRepository.Fill(ref fieRefEntity))
+				.Returns(true);
+
+			#region Assertion
+
+			var moqEntity = moqRefEntity;
+			MoqRepository.Object.Fill(ref moqEntity).Should().Be(true);
+			moqEntity = null;
+			MoqRepository.Object.Fill(ref moqEntity).Should().Be(false);
+
+			var nsubEntity = nsubRefEntity;
+			NsubRepository.Fill(ref nsubEntity).Should().Be(true);
+			nsubEntity = null;
+			NsubRepository.Fill(ref nsubEntity).Should().Be(false);
+
+			var fieEntity = fieRefEntity;
+			FieRepository.Fill(ref fieEntity).Should().Be(true);
+			fieEntity = null;
+			FieRepository.Fill(ref fieEntity).Should().Be(false);
+
+			#endregion
+		}
+	}
+}
